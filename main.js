510335eacf7fb6b8ad8197654717fa50a0f1c537
unknown <jim@home.com>
2015-11-06 06:56:34 +0800

    sample chapter 5

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ cc.game.onStart = function(){
     cc.view.resizeWithBrowserSize(true);
     //load resources
     cc.LoaderScene.preload(g_resources, function () {
-        cc.director.runScene(new ActionScene());
+        cc.director.runScene(new ControlScene());
     }, this);
 };
 cc.game.run();
--- src/app.js
+++ src/app.js
@@ -1,243 +1,216 @@
+function trace(){
+    cc.log(Array.prototype.join.call(arguments, ", "));
+}
 
-var BallLayer = cc.Layer.extend({
-
-    deltaX:1,
-    ball:null,
-    frame:0,
-    bg:null,
-
+var MouseEventLayer = cc.Layer.extend({
     ctor:function () {
         this._super();
 
-        var size = cc.director.getWinSize();
-        var ball = new cc.Sprite("res/item_2.png");
-        ball.x = 0;
-        ball.y = size.height/2;
-        this.addChild(ball, 1);
-        this.ball = ball;
-
-        this.bg = new cc.DrawNode();      //用于记录球的运动轨迹
-        this.addChild(this.bg);
-
-        this.scheduleUpdate();
-        return true;
-    },
-
-    update: function () {
-        var size = cc.director.getWinSize();
-        this.ball.x += this.deltaX;
-        if(this.ball.x >= size.width || this.ball.x <= 0){
-            this.deltaX *= -1;
+        if( 'mouse' in cc.sys.capabilities ) {
+            cc.eventManager.addListener({
+                event: cc.EventListener.MOUSE,
+                onMouseDown: function(event){
+                    var pos = event.getLocation();
+                    var target = event.getCurrentTarget();
+                    if(event.getButton() === cc.EventMouse.BUTTON_RIGHT)
+                        trace("onRightMouseDown at: " + pos.x + " " + pos.y );
+                    else if(event.getButton() === cc.EventMouse.BUTTON_LEFT)
+                        trace("onLeftMouseDown at: " + pos.x + " " + pos.y );
+                },
+                onMouseMove: function(event){
+                    var pos = event.getLocation(), target = event.getCurrentTarget();
+                    trace("onMouseMove at: " + pos.x + " " + pos.y );
+                },
+                onMouseUp: function(event){
+                    var pos = event.getLocation(), target = event.getCurrentTarget();
+                    trace("onMouseUp at: " + pos.x + " " + pos.y );
+                }
+            }, this);
+        } else {
+            trace("MOUSE Not supported");
         }
-        this.ball.y = Math.sin(this.frame/20)*50 + size.height/2;
-
-        this.bg.drawDot(new cc.Point(this.ball.x, this.ball.y), 2, cc.color(255,0,0));  //把球的运动轨迹画到背景板上
-        this.frame++;
-    }
-});
-
-
-var SimpleActionLayer = cc.Layer.extend({
-
-    ctor:function () {
-        this._super();
-
-        var size = cc.director.getWinSize();
-        var ball = new cc.Sprite("res/item_2.png");
-        ball.x = 0;
-        ball.y = size.height/2;
-        this.addChild(ball, 1);
-
-//        var action = cc.moveTo(2, cc.p(size.width, size.height/2));
-//        var action = cc.moveBy(1, cc.p(size.width, 100));
-        ball.x = size.width/2;
-//        var action = cc.scaleTo(1, 2, 2);
-//        var action = cc.scaleBy(1, 2, 2);
-//        ball.runAction(action);
-
-        //镜面测试
-//        ball.scale = 2;
-//        var action = cc.scaleTo(2, -2, 2);
-//        ball.runAction(action);
-
-//        var action = cc.fadeTo(2, 100);
-//        var action = cc.fadeOut(2);
-//        ball.opacity = 0;
-//        var action = cc.fadeIn(2);
-//        ball.runAction(action);
-
-//        var action = cc.blink(2, 10);
-//        ball.runAction(action);
-
-        var action1 = cc.tintTo(0.3, 100, 0, 0);
-        var action2 = cc.tintTo(0.3, 255, 255, 255);
-        ball.runAction(cc.sequence(action1, action2));
 
         return true;
     }
 });
 
-
-var ComposeActionLayer = cc.Layer.extend({
-
+var TouchOneByOneLayer = cc.Layer.extend({
     ctor:function () {
         this._super();
 
-        var size = cc.director.getWinSize();
-        var ball = new cc.Sprite("res/item_2.png");
-        ball.x = 0;
-        ball.y = size.height/2;
-        this.addChild(ball, 1);
-
-//        var action1 = cc.moveTo(2, cc.p(size.width/2, size.height/2));
-//        var action2 = cc.scaleTo(1, 2, 2);
-//        var sequence1 = cc.sequence(action1,action2);
-//        var action3 = cc.scaleTo(1, 1, 1);
-//        var sequence2 = cc.sequence(sequence1, action3);
-//        ball.runAction(sequence2);
-
-//        ball.x = size.width/2;
-//        var action1 = cc.scaleTo(1, 2, 2);
-//        var action2 = cc.scaleTo(1, 1, 1);
-//        var sequence = cc.sequence(action1, action2);
-//        var repeat = cc.repeat(sequence, 5);
-//        var repeat = cc.repeatForever(sequence);
-//        ball.runAction(repeat);
-
-//        var action1 = cc.moveTo(2, cc.p(size.width/2, size.height/2));
-//        var action2 = cc.scaleTo(2, 2, 2);
-//        var spawn = cc.spawn(action1, action2);
-//        ball.runAction(spawn);
-
-//        var action1 = cc.moveTo(2, cc.p(size.width/2, size.height/2));
-//        var reverseTime = cc.reverseTime(action1);
-//        ball.runAction(reverseTime);
-
-//        var action = cc.moveBy(2, cc.p(size.width/2, 0));
-//        var reverse = cc.reverseTime(action);
-////        var reverse = action.reverse();
-//        var sequence = cc.sequence(action, reverse);
-//        ball.runAction(sequence);
-
-        var action1 = cc.moveBy(5, cc.p(size.width/2, 0));
-        var action2 = cc.scaleBy(1, 2);
-        var reverse = action2.reverse();
-        var sequence = cc.sequence(action2, cc.delayTime(0.5), reverse);
-        var repeat = cc.repeat(sequence, 2);
-        var spawn = cc.spawn(action1, repeat);
-        ball.runAction(spawn);
-
-        return true;
-    }
-});
-
-
-var ControlActionLayer = cc.Layer.extend({
-
-    ctor:function () {
-        this._super();
-
-        var size = cc.director.getWinSize();
-        var ball = new cc.Sprite("res/item_2.png");
-        ball.x = 0;
-        ball.y = size.height/2;
-        this.addChild(ball, 1);
-
-//        var action = cc.moveBy(3, cc.p(size.width/2, 0));
-//        action.tag = 123;
-//        ball.runAction(action);
-//        setTimeout(function(){
-//            ball.stopActionByTag(123);
-//        }, 2000);
-
-        var action = cc.moveBy(3, cc.p(size.width/2, 0));
-        ball.runAction(action);
-        setTimeout(function(){
-            ball.pause();
-        }, 2000);
-        setTimeout(function(){
-            ball.resume();
-        }, 3000);
-
-        cc.director.pause();
-        cc.director.resume();
-
-//        var action = cc.moveBy(1, cc.p(size.width/2, 0));
-//        var callback = cc.callFunc(this.callback, this, "message");
-//        var sequence = cc.sequence(action, callback);
-//        ball.runAction(sequence);
+        if( 'touches' in cc.sys.capabilities ) {
+            cc.eventManager.addListener({
+                event: cc.EventListener.TOUCH_ONE_BY_ONE,
+                swallowTouches: true,
+                onTouchBegan: this.onTouchBegan,
+                onTouchMoved: this.onTouchMoved,
+                onTouchEnded: this.onTouchEnded,
+                onTouchCancelled: this.onTouchCancelled
+            }, this);
+        } else {
+            trace("TOUCH-ONE-BY-ONE test is not supported on desktop");
+        }
 
         return true;
     },
-    
-    callback: function (nodeExecutingAction, data) {
-        trace(nodeExecutingAction instanceof cc.Sprite, data);
+
+    onTouchBegan:function(touch, event) {
+        var pos = touch.getLocation();
+        var id = touch.getID();
+        trace("onTouchBegan at: " + pos.x + " " + pos.y + " Id:" + id );
+        var winSize = cc.director.getWinSize();
+        if( pos.x < winSize.width/2) {
+            return true;
+        }
+        return false;
+    },
+    onTouchMoved:function(touch, event) {
+        var pos = touch.getLocation();
+        var id = touch.getID();
+        trace("onTouchMoved at: " + pos.x + " " + pos.y + " Id:" + id );
+    },
+    onTouchEnded:function(touch, event) {
+        var pos = touch.getLocation();
+        var id = touch.getID();
+        trace("onTouchEnded at: " + pos.x + " " + pos.y + " Id:" + id );
+    },
+    onTouchCancelled:function(touch, event) {
+        var pos = touch.getLocation();
+        var id = touch.getID();
+        trace("onTouchCancelled at: " + pos.x + " " + pos.y + " Id:" + id );
+    }
+});
+
+var TouchAllAtOnceLayer = cc.Layer.extend({
+    ctor:function () {
+        this._super();
+
+        if( 'touches' in cc.sys.capabilities ) {
+            cc.eventManager.addListener({
+                event: cc.EventListener.TOUCH_ALL_AT_ONCE,
+                onTouchesBegan: this.onTouchesBegan,
+                onTouchesMoved: this.onTouchesMoved,
+                onTouchesEnded: this.onTouchesEnded,
+                onTouchesCancelled: this.onTouchesCancelled
+            }, this);
+        } else {
+            trace("TOUCHES not supported");
+        }
+    },
+
+    onTouchesBegan:function(touches, event) {
+        for (var i=0; i < touches.length;i++ ) {
+            var touch = touches[i];
+            var pos = touch.getLocation();
+            var id = touch.getID();
+            trace("Touch #" + i + ". onTouchesBegan at: " + pos.x + " " + pos.y + " Id:" + id);
+        }
+    },
+    onTouchesMoved:function(touches, event) {
+        for (var i=0; i < touches.length;i++ ) {
+            var touch = touches[i];
+            var pos = touch.getLocation();
+            var id = touch.getID();
+            trace("Touch #" + i + ". onTouchesMoved at: " + pos.x + " " + pos.y + " Id:" + id);
+        }
+    },
+    onTouchesEnded:function(touches, event) {
+        for (var i=0; i < touches.length;i++ ) {
+            var touch = touches[i];
+            var pos = touch.getLocation();
+            var id = touch.getID();
+            trace("Touch #" + i + ". onTouchesEnded at: " + pos.x + " " + pos.y + " Id:" + id);
+        }
+    },
+    onTouchesCancelled:function(touches, event) {
+        for (var i=0; i < touches.length;i++ ) {
+            var touch = touches[i];
+            var pos = touch.getLocation();
+            var id = touch.getID();
+            trace("Touch #" + i + ". onTouchesCancelled at: " + pos.x + " " + pos.y + " Id:" + id);
+        }
     }
 });
 
 
-var TrickyActionLayer = cc.Layer.extend({
+var KeyboardTestLayer = cc.Layer.extend({
 
     ctor: function () {
         this._super();
-
-        var size = cc.director.getWinSize();
-        var ball = new cc.Sprite("res/item_2.png");
-        ball.x = size.width/2;
-        ball.y = size.height;
-        this.addChild(ball, 1);
-
-        var action = cc.moveBy(2, 0, -(size.height-ball.height/2));
-        action.easing(cc.easeIn(2));
-        var back = action.clone().reverse();
-        back.easing(cc.easeBounceIn());
-        ball.runAction(cc.sequence(action, back));
+        if( 'keyboard' in cc.sys.capabilities ) {
+            cc.eventManager.addListener({
+                event: cc.EventListener.KEYBOARD,
+                onKeyReleased: function(keyCode, event) {
+                    if (keyCode == cc.KEY.back) {
+                        cc.log("return button clicked. keycode:" + keyCode);
+                        cc.director.end();
+                    }
+                    else if (keyCode == cc.KEY.menu) {
+                        cc.log("menu button clicked. keycode:" + keyCode);
+                    }
+                }
+            }, this);
+        } else {
+            cc.log("KEYBOARD Not supported");
+        }
     }
 });
 
 
-var ActionScene = cc.Scene.extend({
-
-    onEnter: function () {
+var AccelerometerLayer = cc.Layer.extend({
+    ctor:function () {
         this._super();
 
-        //每个layer对应不同章节的内容，请自行去掉注释
-//        var layer = new BallLayer();
-//        var layer = new SimpleActionLayer();
-//        var layer = new ComposeActionLayer();
-//        var layer = new ControlActionLayer();
-        var layer = new TrickyActionLayer();
+        var winSize = cc.director.getWinSize();
+
+        if( 'accelerometer' in cc.sys.capabilities ) {
+            cc.inputManager.setAccelerometerInterval(1/30);
+            cc.inputManager.setAccelerometerEnabled(true);
+            cc.eventManager.addListener({
+                event: cc.EventListener.ACCELERATION,
+                callback: function(accelerometerInfo, event){
+                    var target = event.getCurrentTarget();
+                    cc.log('Accel x: '+ accelerometerInfo.x + ' y:' + accelerometerInfo.y + ' z:' + accelerometerInfo.z + ' time:' + accelerometerInfo.timestamp );
+
+                    var w = winSize.width;
+                    var h = winSize.height;
+
+                    var x = w * accelerometerInfo.x + w/2;
+                    var y = h * accelerometerInfo.y + h/2;
+
+                    x = x*0.2 + target.prevX*0.8;       //使小球慢慢移动到目标位置
+                    y = y*0.2 + target.prevY*0.8;
+
+                    target.prevX = x;
+                    target.prevY = y;
+                    target.sprite.x = x;
+                    target.sprite.y = y ;
+                }
+            }, this);
+
+            var sprite = this.sprite = new cc.Sprite("res/item_2.png");
+            this.addChild( sprite );
+            sprite.x = winSize.width/2;
+            sprite.y = winSize.height/2;
+
+            this.prevX = 0;
+            this.prevY = 0;
+        } else {
+            cc.log("ACCELEROMETER not supported");
+        }
+    }
+});
+
+
+
+var ControlScene = cc.Scene.extend({
+    onEnter:function () {
+        this._super();
+//        var layer = new MouseEventLayer();
+//        var layer = new TouchOneByOneLayer();
+//        var layer = new KeyboardTestLayer();
+        var layer = new AccelerometerLayer();
 
         this.addChild(layer);
     }
 });
-
-
-var trace = function() {
-    cc.log(Array.prototype.join.call(arguments, ", "));
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
