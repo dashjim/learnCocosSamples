0d8e4594ec5d2feddeae233fd400f44fab2c91ec
unknown <jim@home.com>
2015-11-06 07:03:53 +0800

    sample chapter 11

--- src/app.js
+++ src/app.js
@@ -2,165 +2,99 @@ function trace(){
     cc.log(Array.prototype.join.call(arguments, ", "));
 }
 
-var SpriteFrameLayer = cc.Layer.extend({
-
-    ctor: function () {
+var AnimationLayer = cc.Layer.extend({
+    ctor:function () {
         this._super();
-        cc.spriteFrameCache.addSpriteFrames("res/candy.plist");
+
         var size = cc.winSize;
-        var ball = new cc.Sprite("#1.png");
-        this.addChild(ball, 1);
-        ball.x = size.width/2;
-        ball.y = size.height/2;
-        return true;
-    }
-});
-
-var BatchNodeLayer = cc.Layer.extend({
-
-    ctor: function () {
-        this._super();
-        cc.spriteFrameCache.addSpriteFrames("res/candy.plist");
-
-        var batchNode = new cc.SpriteBatchNode("res/candy.png");
-        this.addChild(batchNode);
-        var size = cc.winSize;
-        for (var i = 0; i < 4000; i++) {
-            var ball = new cc.Sprite("#"+ (parseInt(Math.random()*5)+1) +".png");
-            batchNode.addChild(ball, 1);
-            ball.x = Math.random()*size.width;
-            ball.y = Math.random()*size.height;
-            ball.runAction(cc.rotateBy(1, 360*Math.random(), 360*Math.random()).repeatForever());
+        var man = new cc.Sprite();
+        var animation = new cc.Animation();
+        for (var i = 0; i < animation_pngs.length; i++) {
+            animation.addSpriteFrameWithFile(animation_pngs[i]);
         }
-        return true;
+        animation.setDelayPerUnit(1/14);
+        var action = cc.animate(animation);
+        action.repeatForever();
+        man.runAction(action);
+        this.addChild(man);
+        man.x = size.width/2;
+        man.y = size.height/2;
     }
 });
 
-var BatchNodeContrastLayer = cc.Layer.extend({
-
-    ctor: function () {
+var ArmatureLayer = cc.Layer.extend({
+    _armature:null,
+    _direction:1,
+    onEnter:function () {
         this._super();
-        cc.spriteFrameCache.addSpriteFrames("res/candy.plist");
 
-        var size = cc.winSize;
+        var winSize = cc.winSize;
+        ccs.armatureDataManager.addArmatureFileInfo("res/DemoPlayer/DemoPlayer.ExportJson");
+        this._armature = new ccs.Armature("DemoPlayer");
+        this._armature.getAnimation().play("walk_fire");
+        this._armature.scaleX = -0.25;
+        this._armature.scaleY = 0.25;
+        this._armature.x = winSize.width / 2 - 150;
+        this._armature.y = winSize.height / 2;
+        this._armature.getAnimation().setMovementEventCallFunc(this.animationEventHandler,this);
+        this.addChild(this._armature);
 
-        for (var i = 0; i < 4000; i++) {
-            var ball = new cc.Sprite("#"+ (parseInt(Math.random()*5)+1) +".png");
-            this.addChild(ball, 1);
-            ball.x = Math.random()*size.width;
-            ball.y = Math.random()*size.height;
-            ball.runAction(cc.rotateBy(1, 360*Math.random(), 360*Math.random()).repeatForever());
-        }
-        return true;
-    }
-});
-
-var ReuseSprite = cc.Sprite.extend({
-    ctor: function (url) {
-        this._super(url);
+        this._direction = 1;
     },
 
-    reuse: function (param) {
-        trace("reuse", param);
-    },
+    animationEventHandler:function (armature, movementType, movementID) {
+        if (movementType == ccs.MovementEventType.loopComplete) {
+            if (movementID == "walk_fire") {
+                var moveBy = cc.moveBy(2, cc.p(300 * this._direction, 0));
+                this._armature.stopAllActions();
+                this._armature.runAction(cc.sequence(moveBy, cc.callFunc(this.callback, this)));
+                this._armature.getAnimation().play("walk");
 
-    unuse: function () {
-        trace("unuse");
-    }
-});
-
-var PoolLayer = cc.Layer.extend({
-    tag:0,
-    deleteTag:0,
-    ctor: function () {
-        this._super();
-        cc.spriteFrameCache.addSpriteFrames("res/candy.plist");
-        deleteTag = tag = 0;
-        this.scheduleUpdate();
-    },
-
-    update: function () {
-        var size = cc.winSize;
-        if(this.tag - this.deleteTag > 500){
-            for (var i = 0; i < 250; i++) {
-                var ball = this.getChildByTag(this.deleteTag);
-                cc.pool.putInPool(ball);
-                this.removeChild(ball);
-                this.deleteTag++;
+                this._direction *= -1;
             }
         }
-        var param = "anything";
-        for (var i = 0; i < 250; i++) {
-            var ball = null;
-            if(cc.pool.hasObject(ReuseSprite)){
-                ball = cc.pool.getFromPool(ReuseSprite, param);
-            } else {
-                ball = new ReuseSprite("#" + (parseInt(Math.random() * 5) + 1) + ".png");
-            }
-            this.addChild(ball, 1, this.tag);
-            this.tag++;
-            ball.x = Math.random()*size.width;
-            ball.y = Math.random()*size.height;
-        }
-    }
-});
-
-var PoolContrastLayer = cc.Layer.extend({
-    tag:0,
-    deleteTag:0,
-    ctor: function () {
-        this._super();
-        cc.spriteFrameCache.addSpriteFrames("res/candy.plist");
-        deleteTag = tag = 0;
-        this.scheduleUpdate();
     },
 
-    update: function () {
-        var size = cc.winSize;
-        if(this.tag - this.deleteTag > 500){
-            for (var i = 0; i < 250; i++) {
-                this.removeChildByTag(this.deleteTag, false);
-                this.deleteTag++;
-            }
-        }
-        for (var i = 0; i < 250; i++) {
-            var ball = new cc.Sprite("#"+ (parseInt(Math.random()*5)+1) +".png");
-            this.addChild(ball, 1, this.tag);
-            this.tag++;
-            ball.x = Math.random()*size.width;
-            ball.y = Math.random()*size.height;
-        }
+    callback:function () {
+        this._armature.runAction(cc.scaleTo(0.1, 0.25 * this._direction * -1, 0.25));
+        this._armature.getAnimation().play("walk_fire", 10);
     }
 });
 
-var BakeLayer = cc.Layer.extend({
+
+var DragonBonesLayer = cc.Layer.extend({
 
     ctor: function () {
         this._super();
 
-        cc.spriteFrameCache.addSpriteFrames("res/candy.plist");
-        var size = cc.winSize;
-        var layer = new cc.Layer();
-        this.addChild(layer);
-        for (var i = 0; i < 8000; i++) {
-            var node = new cc.Sprite("#"+ (parseInt(Math.random()*5)+1) +".png");
-            node.x = Math.random()*size.width;
-            node.y = Math.random()*size.height + 200;
-            layer.addChild(node);
-        }
-        layer.bake();
+        ccs.armatureDataManager.addArmatureFileInfo("res/dragonbones/skeleton.png", "res/dragonbones/skeleton.plist", "res/dragonbones/skeleton.xml");
+        var armature = new ccs.Armature("Dragon");
+        armature.getAnimation().play("walk");
+        armature.getAnimation().setSpeedScale(24/60);
+        this.addChild(armature);
+        armature.x = cc.winSize.width/2;
+        armature.y = cc.winSize.height/2;
     }
 });
+
 
 var GameScene = cc.Scene.extend({
     onEnter:function () {
         this._super();
-//        var layer = new SpriteFrameLayer();
-//        var layer = new BatchNodeLayer();
-//        var layer = new BatchNodeContrastLayer();
-//        var layer = new PoolContrastLayer();
-//        var layer = new PoolLayer();
-        var layer = new BakeLayer();
+//        var layer = new AnimationLayer();
+//        var layer = new ArmatureLayer();
+        var layer = new DragonBonesLayer();
         this.addChild(layer);
     }
 });
+
+
+
+
+
+
+
+
+
+
+
